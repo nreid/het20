4326c8e1c4f67ac6a807e447a0ce312cf9a04042
Noah Reid <noah.reid@uconn.edu>
2020-02-17 09:35:06 -0500

    trim test

--- scripts/trim_test/01_trim_align.sh
+++ scripts/trim_test/01_trim_align.sh
@@ -42,7 +42,7 @@ SAMPLES=($(echo RLM_14517 RLM_14540 RLM_14565 RLM_14573 RLM_14948 BU18FLJX053 BU
 	# run trimmomatic
 	java -jar $Trimmomatic PE \
 	-threads 12 \
-	-basein $INFILE1 $INFILE2 \
+	$INFILE1 $INFILE2 \
 	-baseout $TRIMDIR/$OUTFILE \
 	ILLUMINACLIP:$ADAPT:2:30:10 \
 	LEADING:3 \
