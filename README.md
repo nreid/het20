7faf376bcb887f5a0de77e2a01bbc5c0382d3dd2
Noah Reid <noah.reid@uconn.edu>
2020-02-17 14:22:56 -0500

    concat

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # alignment and variant calling on new heteroclitus WGS data
 
-- ref genome is Reid et al. 2017
-- ref genome, sequencing data are symlinked to genome, data directories
-- older WGS from Reid et al. 2016 and Oziolor et al. 2019 aligned to the same reference are used here. They're symlinked into results/alignment
+- ref genome is Reid et al. 2017. symlinked to `/genome` directory
+
+three sources of data:
+
+1. new data from collaborators Matson and Mayden
+	- data symlinked to `/data` directory
+2. Reid et al. 2016 data
+    - three low cov populations are already aligned. bam files symlinked to `/results/alignments`directory in project
+    - two 6x coverage populations need to have fastqs concatenated, aligned. scripts to do this will read and write OUTSIDE the project directory. resulting bam files will be symlinked to `/results/alignments` directory
+3. Oziolor et al. 2019
+	- all samples already aligned. symlinked to `/results/alignments` directory
+
+
+new data has notable adapter contamination. 
+
+	- comparing variant calls on 7 samples with/out trimming. 
---
+++ scripts/align/03_x_concatenateBINBH.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=concatenateBINBH
+#SBATCH -o %x_%A_%a.out
+#SBATCH -e %x_%A_%a.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --array=[0-383]%50
+
+
+# concatenate fastq files for BI and NBH populations prior to aligning. 
+# these fastq files exist OUTSIDE the project directory
+# this script should not be executed again. 
+# a following script will align, again writing OUTSIDE the project directory
+# bam files will be symlinked to /results/alignments
+
+INDIR=../../../data/Reidetal2016_raw
+OUTDIR=../../../data/Reidetal2016_merged
+mkdir -p OUTDIR
+
+# get sample IDs
+SAMPLES=($(find $INDIR -name "*fastq.gz" | sed 's/.*\///' | sed 's/_[ACGT][ACGT].*//' | grep -v "_" | sort -V | uniq))
+
+# get sample for this task
+SAM=${SAMPLES[$SLURM_ARRAY_TASK_ID]}
+
+# get array of fastq files for R1, R2
+FQ1S=($(find $INDIR -name "${SAM}_*fastq.gz" | grep R1 | sort))
+FQ2S=($(find $INDIR -name "${SAM}_*fastq.gz" | grep R2 | sort))
+
+# concatenate R1
+cat ${FQ1S[@]} >TEST/$SAM.merged_R1.fastq.gz
+# concatenate R2
+cat ${FQ2S[@]} >TEST/$SAM.merged_R2.fastq.gz
