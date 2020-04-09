f54e6a42ed6cba0b6a953603b67688e8c63781d6
Noah Reid <noah.reid@uconn.edu>
2020-04-09 15:14:41 -0400

    dbimport

--- scripts/variants/02_gatk_db.sh
+++ scripts/variants/02_gatk_db.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -c 7
-#SBATCH --mem=15G
+#SBATCH --mem=30G
 #SBATCH --qos=general
 #SBATCH --partition=general
 #SBATCH --mail-user=
@@ -28,10 +28,11 @@ GVCFS=$(ls ${INDIR}/*g.vcf | sed 's/^/-V /' | tr "\n" "\t")
 
 
 #IMPORTANT: The -Xmx value the tool is run with should be less than the total amount of physical memory available by at least a few GB, as the native TileDB library requires additional memory on top of the Java memory. Failure to leave enough memory for the native code can result in confusing error messages!
-gatk --java-options "-Xmx10g -Xms4g" GenomicsDBImport \
+gatk --java-options "-Xmx30g -Xms4g" GenomicsDBImport \
   $GVCFS \
   --genomicsdb-workspace-path $OUTDIR \
   --overwrite-existing-genomicsdb-workspace true \
+  --batch-size 50 \
   -L NW_012224401.1
 
 date
