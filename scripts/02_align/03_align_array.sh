3ab4c8eba662558e9de66a118e31686582153b4c
Noah Reid <noah.reid@uconn.edu>
2020-12-16 14:26:41 -0500

    sra2sampleid

---
+++ scripts/02_align/03_align_array.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=bwa
+#SBATCH -o %x_%A_%a.out
+#SBATCH -e %x_%A_%a.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=5G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --array=[0-885]%30
+
+
+hostname
+date
+
+echo "host name : " `hostname`
+echo This is array task number $SLURM_ARRAY_TASK_ID
+
+# load software----------------------------------------------
+module load samtools/1.10
+module load bwa/0.7.17
+module load samblaster/0.1.24
+
+# set input/output files, directories------------------------
+
+# input fastq file directory
+INDIR=../../data
+
+# output directory
+OUTDIR=../../results/alignments
+mkdir -p $OUTDIR
+
+# reference genome
+GENOME=../../genome/mummichog
+
+# fastq file bash array
+FQS=($(find -L ${INDIR} -name "*1.fastq.gz"))
+
+# fastq files
+FQ1=${FQS[$SLURM_ARRAY_TASK_ID]}
+
+# execute alignment script, supplying fastq R1, output directory, and reference genome
+bash align_generic.sh $FQ1 $OUTDIR $GENOME
